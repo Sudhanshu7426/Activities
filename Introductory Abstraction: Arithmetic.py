47672e1e5ddf8a4a8a23d38ad1eddf6a90f86b17
Sudhanshu7426 <124574081+Sudhanshu7426@users.noreply.github.com>
2023-04-12 02:58:47 +0530

    main

---
+++ Introductory Abstraction: Arithmetic.py
@@ -0,0 +1,30 @@
+sp=int(input())
+cp=int(input())
+def add(sp,cp):
+    c=sp+cp
+    return c
+print("Addition:",add(sp,cp))
+def subtract(sp,cp):
+    c=sp-cp
+    return c
+print("Subtraction:",subtract(sp,cp))
+def multiply(sp,cp):
+    c=sp*cp
+    return c
+print("Multiplication:",multiply(sp,cp))
+def divide(sp,cp):
+    c=sp/cp
+    return c
+print("Division:",divide(sp,cp))
+def power(sp,cp):
+    c=sp**cp
+    return c
+print("Power:",power(sp,cp))
+def remainder(sp,cp):
+    c=sp%cp
+    return c
+print("Remainder:",remainder(sp,cp))
+def floor_division(sp,cp):
+    c=sp+cp
+    return c
+print("Floor division:",floor_division(sp,cp))
