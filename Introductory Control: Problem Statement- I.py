d3ba90e016065a0d96b1a4148070a6b21b9d4a4a
Sudhanshu7426 <124574081+Sudhanshu7426@users.noreply.github.com>
2023-04-12 03:11:23 +0530

    main

---
+++ Introductory Control: Problem Statement- I.py
@@ -0,0 +1,19 @@
+r=1
+print("Input for team 1")
+while r<=2:
+    team_scored = int(input("Enter the total runs scored by Team 1: "))
+    team_faced = int(input("Enter the total overs faced by Team : "))
+    team_conceded = int(input("Enter the total runs conceded by Team : "))
+    team_bowled= int(input("Enter the total overs bowled by Team : "))
+    if r==1:
+        team1_Nrr = (team_scored / team_faced) - (team_conceded / team_bowled)
+        print("Input for team 2")
+    else:
+        team2_Nrr = (team_scored / team_faced) - (team_conceded / team_bowled)
+    r+=1
+if team1_Nrr > team2_Nrr:
+    print("Team 1 has a higher Net Run Rate and tops the points table.")
+elif team2_Nrr > team1_Nrr:
+    print("Team 2 has a higher Net Run Rate and tops the points table.")
+else:
+    print(" Match Tie")
